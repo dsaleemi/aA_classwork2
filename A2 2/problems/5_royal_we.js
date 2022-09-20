8e33ad8a306caa12d31605b3109aba9cf2a56251
Daniel Saleemi <daniellesaleemi@gmail.com>
2022-09-20 19:42:48 -0400

    testing

---
+++ A2 2/problems/5_royal_we.js
@@ -0,0 +1,30 @@
+/*******************************************************************************
+Write a function royalWe(sentence) that returns an string where every word
+'I' is replaced with 'we', every word 'mine' is replaced with 'ours', every
+word 'my' is replaced with 'our', and every word 'me' is replaced with "us"
+
+Examples:
+
+royalWe("I want to go to the store") => "we want to go to the store"
+royalWe("This is my house and you will respect me") => "This is our house and you will respect us"
+royalWe("This is mine") => "This is ours"
+royalWe("Jump for my love") => "Jump for our love"
+*******************************************************************************/
+
+
+function royalWe(sentence) {
+
+  let newArr = [];
+  let words = sentence.split;
+
+  for (let i = 0; i < words; i++) {
+    let newWords = sentence[i];
+
+    if (newWords === 'I') {
+      return
+    }
+  }
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
+module.exports = royalWe;
